e4d2b764182ff3f2efda5005ccccae3abf586b72
Fabian <garciafabianemmanuel@gmail.com>
2025-07-03 01:12:46 -0300

    factory fuction

--- src/app.js
+++ src/app.js
@@ -1,2 +1,15 @@
 const { getAge, getUUID } = require('./plugins');
 const { buildMakePerson } = require('./FactoryFuction/factory.js');
+const { log } = require('console');
+
+
+const makePerson = buildMakePerson({ getUUID, getAge });
+
+
+const obj = {
+    name: 'John',
+    birthdate: '1985-10-21'
+};
+
+const john = makePerson(obj);
+console.log({ john });
